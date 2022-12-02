71a3ae551105519207ce3324c1e8fa3c25e271b3
Manuel Stausberg <stausberg@denic.de>
2022-12-02 20:11:19 +0100

    refactor day 1

--- 01/main.py
+++ 01/main.py
@@ -1,6 +1,5 @@
 INPUT_FILE = "input.txt"
 
-
 elf_list = []
 elf_tmp = []
 
@@ -10,13 +9,10 @@ with open(INPUT_FILE, mode="r") as infile:
             item_cals = int(line.strip())
             elf_tmp.append(item_cals)
         except ValueError:
-            elf_list.append(elf_tmp)
-            elf_tmp = []
+            elf_list.append(sum(elf_tmp))
+            elf_tmp.clear()
     # Append last elf before closing filehandle
-    elf_list.append(elf_tmp)
+    elf_list.append(sum(elf_tmp))
 
-elf_sums = [sum(elf) for elf in elf_list]
-
-print(f"Max: {max(elf_sums)}")
-
-print(f"Top 3: {sum(sorted(elf_sums, reverse=True)[:3])}")
+print(f"Max: {max(elf_list)}")
+print(f"Top 3: {sum(sorted(elf_list, reverse=True)[:3])}")
