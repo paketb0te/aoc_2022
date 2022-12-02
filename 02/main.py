dc4344b2a1b772e91bea99d2b673c38e6524647f
Manuel Stausberg <stausberg@denic.de>
2022-12-02 18:47:40 +0100

    Day 2 Part 2 and refactoring

--- 02/main.py
+++ 02/main.py
@@ -11,18 +11,39 @@ Paper vs Scissors:  -1      -> b
 Scissors vs Rock:    2      -> b
 Scissors vs Paper:   1      -> a
 Scissors vs Scissor: 0      -> draw
-"""
 
+3 > 2 > 1 > 3
+"""
 from enum import Enum
+from typing import Callable
+
+INPUT_FILE = "test_input.txt"
+# INPUT_FILE = "input.txt"
 
 
 class Shape(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+    ROCK = 0
+    PAPER = 1
+    SCISSORS = 2
 
 
-MAPPING = {
+def get_shape(code: str) -> Shape:
+    return CODE_MAPPING_PART_1[code]
+
+
+def get_winning_move(shape: Shape) -> Shape:
+    return Shape((shape.value + 1) % 3)
+
+
+def get_drawing_move(shape: Shape) -> Shape:
+    return Shape(shape.value)
+
+
+def get_losing_move(shape: Shape) -> Shape:
+    return Shape((shape.value + 2) % 3)
+
+
+CODE_MAPPING_PART_1 = {
     "A": Shape.ROCK,
     "B": Shape.PAPER,
     "C": Shape.SCISSORS,
@@ -31,19 +52,31 @@ MAPPING = {
     "Z": Shape.SCISSORS,
 }
 
+CODE_MAPPING_PART_2 = {
+    "X": get_losing_move,
+    "Y": get_drawing_move,
+    "Z": get_winning_move,
+}
 
-def get_shape(code: str) -> Shape:
-    return MAPPING[code]
+
+def get_moves_part_1(code_1: str, code_2: str) -> tuple[Shape, Shape]:
+    return get_shape(code=code_1), get_shape(code=code_2)
 
 
-def evaluate_round(p1: str, p2: str) -> tuple[int, int]:
-    v1 = get_shape(code=p1).value
-    v2 = get_shape(code=p2).value
+def get_moves_part_2(code_1: str, code_2: str) -> tuple[Shape, Shape]:
+    s1 = get_shape(code=code_1)
+    s2 = CODE_MAPPING_PART_2[code_2](shape=s1)
+    return s1, s2
 
-    tmp = v1 - v2
-    if tmp in (-2, 1):  # p1 wins
+
+def evaluate_round(s1: Shape, s2: Shape) -> tuple[int, int]:
+    v1 = s1.value + 1
+    v2 = s2.value + 1
+
+    tmp = (v1 - v2 + 3) % 3
+    if tmp == 1:  # p1 wins
         v1 += 6
-    elif tmp in (-1, 2):  # p2 wins
+    elif tmp == 2:  # p2 wins
         v2 += 6
     else:  # draw
         v1 += 3
@@ -52,16 +85,29 @@ def evaluate_round(p1: str, p2: str) -> tuple[int, int]:
     return v1, v2
 
 
-INPUT_FILE = "test_input.txt"
+def get_scores(
+    moves_getter: Callable[[str, str], tuple[Shape, Shape]]
+) -> tuple[int, int]:
+    scores = (0, 0)
+    with open(INPUT_FILE, mode="r") as infile:
+        for line in infile:
+            line = line.strip()
 
-scores = (0, 0)
+            code_1, code_2 = line.split(maxsplit=1)
+            s1, s2 = moves_getter(code_1, code_2)
+            score_1, score_2 = evaluate_round(s1=s1, s2=s2)
+            scores = scores[0] + score_1, scores[1] + score_2
+    return scores
 
-with open(INPUT_FILE, mode="r") as infile:
-    for line in infile:
-        line = line.strip()
 
-        p1, p2 = line.split(maxsplit=1)
-        s1, s2 = evaluate_round(p1=p1, p2=p2)
-        scores = scores[0] + s1, scores[1] + s2
+scores = get_scores(moves_getter=get_moves_part_1)
 
+print("-" * 80)
+print(f"# Part 2:")
+print(f"P1: {scores[0]}\nP2: {scores[1]}")
+
+scores = get_scores(moves_getter=get_moves_part_2)
+
+print("-" * 80)
+print(f"# Part 2:")
 print(f"P1: {scores[0]}\nP2: {scores[1]}")
