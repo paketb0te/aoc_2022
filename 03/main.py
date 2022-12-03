00206a0e20392e22f0e05ebae83e24a517662378
Manuel Stausberg <stausberg@denic.de>
2022-12-03 12:14:20 +0100

    refactor day 3

--- 03/main.py
+++ 03/main.py
@@ -8,30 +8,27 @@ prio_map = dict(zip(string.ascii_lowercase, range(1, 27))) | dict(
 )
 
 prio_sum = 0
-
-with open(INPUT_FILE, mode="r") as infile:
-    for line in infile:
-        line = line.strip()
-        # Get the intersection of the first and second half of the line.
-        item_set = set(line[: len(line) // 2]) & set(line[len(line) // 2 :])
-        # Get the only element in the set and its priority to the total.
-        prio_sum += prio_map[next(iter(item_set))]
-
-print(f"Part 1: {prio_sum}")
-
-# Part 2
-
 badge_prio_sum = 0
 elf_group = []
 
 with open(INPUT_FILE, mode="r") as infile:
     for line in infile:
         line = line.strip()
+
+        # Part 1
+        # Get the intersection of the first and second half of the line.
+        item_set = set(line[: len(line) // 2]) & set(line[len(line) // 2 :])
+        # Get the only element in the set and its priority to the total.
+        prio_sum += prio_map[next(iter(item_set))]
+
+        # Part 2
+        # Add an elf to the group
         elf_group.append(set(line))
+        # process in groups of three
         if len(elf_group) >= 3:
             badge_set = set.intersection(*elf_group)
             badge_prio_sum += prio_map[next(iter(badge_set))]
             elf_group.clear()
 
-
+print(f"Part 1: {prio_sum}")
 print(f"Part 2: {badge_prio_sum}")
